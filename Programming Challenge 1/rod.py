93ac3e085e405d9698972bbac5a743f23a8c275f
zk-2905 <113169587+zk-2905@users.noreply.github.com>
2022-10-14 09:40:03 +0100

    Delete Programming Challenge 1 directory

--- Programming Challenge 1/rod.py
+++
@@ -1,7 +0,0 @@
-
-rod = float(input("rods: "))
-print("Meters ", rod*5.0292)
-print("Furlongs", rod/40)
-print("Mile", ((rod*5.0292)/1609.34))
-print("Foot", ((rod*5.0292)/0.3048))
-print("Minutes to walk", (rod), "rods", ((((rod*5.0292)/1609.34)/3.1)*60))
